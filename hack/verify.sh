7fa6e3b5853a1dc9f21b07a15c790a0fd727f516
Christoph Blecker <admin@toph.ca>
2017-10-24 17:54:29 -0700

    Don't use docker by default for sig docs generator

--- hack/verify.sh
+++ hack/verify.sh
@@ -1,38 +1,44 @@
 #!/bin/bash
 
-export CRTDIR=$(pwd)
-export TMPDIR=/tmp/testgendocs
-mkdir $TMPDIR
+CRT_DIR=$(pwd)
+VERIFY_TEMP=$(mktemp -d 2>/dev/null || mktemp -d -t k8s-community.XXXXXX)
+WORKING_DIR=${VERIFY_TEMP}/src/testgendocs
+GOPATH=${VERIFY_TEMP}
+mkdir -p ${WORKING_DIR}
 
-cp -r sig* Makefile generator $TMPDIR
+function cleanup {
+  rm -rf "${VERIFY_TEMP}"
+}
+trap cleanup EXIT
 
-cd $TMPDIR
+cp -r sig* Makefile generator ${WORKING_DIR}
+
+cd ${WORKING_DIR}
 make 1>/dev/null
 
 mismatches=0
 break=$(printf "=%.0s" $(seq 1 68))
 
-for file in $(ls $CRTDIR/sig-*/README.md $CRTDIR/sig-list.md); do
-  real=${file#$CRTDIR/}
-  if ! diff -q $file $TMPDIR/$real &>/dev/null; then
-    echo "$file does not match $TMPDIR/$real";
+for file in $(ls ${CRT_DIR}/sig-*/README.md ${CRT_DIR}/sig-list.md); do
+  real=${file#$CRT_DIR/}
+  if ! diff -q ${file} ${WORKING_DIR}/${real} &>/dev/null; then
+    echo "${file} does not match ${WORKING_DIR}/${real}";
     mismatches=$((mismatches+1))
   fi;
 done
 
-if [ $mismatches -gt "0" ]; then
+if [ ${mismatches} -gt "0" ]; then
   echo ""
-  echo $break
+  echo ${break}
   noun="mismatch was"
-  if [ $mismatches -gt "0" ]; then
+  if [ ${mismatches} -gt "0" ]; then
     noun="mismatches were"
   fi
-  echo "$mismatches $noun detected."
+  echo "${mismatches} ${noun} detected."
   echo "Do not manually edit sig-list.md or README.md files inside the sig folders."
   echo "Instead make your changes to sigs.yaml and then run \`make\`.";
-  echo $break
+  echo ${break}
   exit 1;
 fi
 
-rm -rf $TMPDIR
 exit 0
