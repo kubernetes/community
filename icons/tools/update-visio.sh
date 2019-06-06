60ab42517181e73307f1916147192aede0498fc4
David Hovey <david@hoveytech.com>
2019-06-06 12:11:35 -0700

    Update update-visio.sh
    Fix for visio script

--- icons/tools/update-visio.sh
+++ icons/tools/update-visio.sh
@@ -3,7 +3,7 @@
 CONTENT_PATH="${PWD}/png"
 OUTPUT_FILENAME="kubernetes-visio-stencil"
 
-curl -s -N https://raw.githubusercontent.com/hoveytechllc/visio-stencil-creator/master/scripts/build-and-run.sh | bash -s --content-path=${CONTENT_PATH} --output-filename=${OUTPUT_FILENAME}
+curl -s -N https://raw.githubusercontent.com/hoveytechllc/visio-stencil-creator/master/scripts/build-and-run.sh | bash -s -- --content-path=${CONTENT_PATH} --output-filename=${OUTPUT_FILENAME}
 
 if [ -f ${CONTENT_PATH}/${OUTPUT_FILENAME}.vssx ]; then
     mv ${CONTENT_PATH}/${OUTPUT_FILENAME} ./visio/${OUTPUT_FILENAME}.vssx
